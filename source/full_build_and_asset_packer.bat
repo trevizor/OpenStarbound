ea6f28118628c6f32ddc938fd079de6dbd1c3c99
Glauber Bonifacio <glauber.bonifacio@gmail.com>
2026-05-18 12:10:20 -0300

    fixed release scripts

--- source/full_build_and_asset_packer.bat
+++ source/full_build_and_asset_packer.bat
@@ -70,8 +70,8 @@ if errorlevel 1 (
 )
 
 echo Copying win files to Steam Starbound win64 directory...
-xcopy /E /Y /I "..\client_distribution\assets\*.*" "H:\SteamLibrary\steamapps\common\Starbound\assets\"
-xcopy /E /Y /I "..\client_distribution\win\*.*" "H:\SteamLibrary\steamapps\common\Starbound\win64\"
+xcopy /E /Y /I "..\client_distribution\assets\*" "H:\SteamLibrary\steamapps\common\Starbound\assets\"
+xcopy /E /Y /I "..\client_distribution\win\*" "H:\SteamLibrary\steamapps\common\Starbound\win64\"
 
 popd
 echo Done: full build and asset packing completed.
